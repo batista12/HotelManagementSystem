b55f9224d265420cde618b78bbe435b267a0bba9
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-24 07:20:17 -0700

    [Manasi]Hotel Management System master branch is  created

---
+++ hotel/src/main/java/com/capgemini/hotel/Hotel.java
@@ -0,0 +1,13 @@
+package com.capgemini.hotel;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hotel 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome" );
+    }
+}
---
+++ hotel/src/test/java/com/capgemini/hotel/HotelTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.hotel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class HotelTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
